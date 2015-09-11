9f3b1e34e4f7cbc94efb23322f79d8a8c7d2a2f7
Dmitry Korolev <dk@mustdigital.ru>
2015-09-11 12:47:36 +0300

    0.5.61 - Fixed wp_editor value.

--- templates/fields/tinymce.php
+++ templates/fields/tinymce.php
@@ -8,7 +8,7 @@ if ( isset($meta['options']['tinymce']) ) {
 <tr>
     <th scope="row"><label for="<?=$meta['id'];?>'"><?=$meta['title'];?></label></th>
     <td>
-        <?php wp_editor( $meta['value'], $meta['id'], $options ); ?>
+        <?php wp_editor( htmlspecialchars_decode($meta['value']), $meta['id'], $options ); ?>
         <?php if ( isset($meta['description']) ) { ?><p class="description"><?=$meta['description'];?></p><?php } ?>
     </td>
 </tr>
